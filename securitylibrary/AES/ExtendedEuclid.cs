9a9451b6a22316b016954f212e1f6f34909823aa
amr614 <amrmohamed28320@gmail.com>
2024-04-23 23:11:13 +0200

    MultiplicativeInverse

--- securitylibrary/AES/ExtendedEuclid.cs
+++ securitylibrary/AES/ExtendedEuclid.cs
@@ -16,7 +16,31 @@ namespace SecurityLibrary.AES
         /// <returns>Mul inverse, -1 if no inv</returns>
         public int GetMultiplicativeInverse(int number, int baseN)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            #region var
+            int Q;
+            int A1=1,A2=0,A3=baseN;
+            int B1=0,B2=1,B3=number;
+            int temp1=B1,temp2=B2,temp3=B3;
+            #endregion
+            while (B3 != 0 && B3 != 1)
+            {
+                Q  = A3/B3;
+                B1 = A1-(Q * temp1);
+                B2 = A2-(Q * temp2);
+                B3 = A3-(Q * temp3);
+                A1 = temp1;
+                A2 = temp2;
+                A3 = temp3;
+                temp1 = B1;
+                temp2 = B2;
+                temp3 = B3;
+            }
+            if (B3 == 0)
+                return -1;
+            if (B2 < 0)
+                B2 += baseN;
+            return B2;
         }
     }
 }
