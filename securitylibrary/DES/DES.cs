c394f254cd9868d5a2d02d0d2f3043cfa9243a89
abdelwahabT <abdoamin588@gmail.com>
2024-04-23 22:11:47 +0200

    DES ENCRYPTION

--- securitylibrary/DES/DES.cs
+++ securitylibrary/DES/DES.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,7 @@ namespace SecurityLibrary.DES
     /// </summary>
     public class DES : CryptographicTechnique
     {
+        #region Tables
         // first IP for the input
         int[] IP = {58,50,42,34,26,18,10,2,
                     60,52,44,36,28,20,12,4,
@@ -53,26 +55,32 @@ namespace SecurityLibrary.DES
                     3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5,
                     0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15,
                     13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9 };
+
         int[] S3 = {10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8,
                     13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1,
                     13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7,
                     1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12 };
+
         int[] S4 = {7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15,
                     13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9,
                     10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4,
                     3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14 };
+
         int[] S5 = {2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9,
                     14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6,
                     4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14,
                     11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3 };
+
         int[] S6 = {12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11,
                     10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8,
                     9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6,
                     4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13 };
+
         int[] S7 = {4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1,
                     13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6,
                     1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2,
                     6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12 };
+
         int[] S8 = {13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7,
                     1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2,
                     7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8,
@@ -96,9 +104,13 @@ namespace SecurityLibrary.DES
                       34, 53, 46, 42, 50, 36, 29, 32 };
         // Schedule of Left Shifts
         int[] Schedule_of_left_shifts = { 1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1 };
+        #endregion
         public override string Decrypt(string cipherText, string key)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+
+
+            return null;
         }
 
         public override string Encrypt(string plainText, string key)
@@ -132,8 +144,33 @@ namespace SecurityLibrary.DES
                 // Round
                 b1_ip = Round(b1_ip, PC_2);
             }
+            // 32 bit swap
+            int[] left_b = new int[32];
+            int[] right_b = new int[32];
+            for (int i = 0; i < 32; i++)
+            {
+                left_b[i] = b1_ip[i];
+                right_b[i] = b1_ip[i + 32];
+            }
+            for (int i = 0; i < 32; i++)
+            {
+                b1_ip[i] = right_b[i];
+                b1_ip[i + 32] = left_b[i];
+            }
+            // inverse initial permutation
+            int[] cipher = new int[64];
+            for (int i = 0; i < 64; i++)
+            {
+                cipher[i] = b1_ip[IP_inverse[i] - 1];
+            }
+            // convert to hex in string
+            string cipher_hex = "0x";
+            for (int i = 0; i < 16; i++)
+            {
+                cipher_hex += Convert.ToString(Convert.ToInt64($"{cipher[(i*4)]}{cipher[(i * 4) + 1]}{cipher[(i * 4) + 2]}{cipher[(i * 4) + 3]}",2), 16);
 
-            return null;
+            }
+            return cipher_hex;
         }
         public int[] convert_to_64block(string S)
         {
@@ -191,7 +228,7 @@ namespace SecurityLibrary.DES
                 int temp2 = T[1];
                 for (int i = 0; i < 26; i++)
                 {
-                    ret[i] = T[i + 1];
+                    ret[i] = T[i + 2];
                 }
                 ret[26] = temp;
                 ret[27] = temp2;
@@ -217,6 +254,7 @@ namespace SecurityLibrary.DES
         }
         public int[] Round(int[] b1, int[] key_pc2)
         {
+            int[] result = new int[64];
             //split b1 to left and right
             int[] left_b = new int[32];
             int[] right_b = new int[32];
@@ -229,7 +267,7 @@ namespace SecurityLibrary.DES
             int[] right_exp = new int[48];
             for (int i = 0; i < 48; i++)
             {
-                right_exp[i] = right_b[E[i]];
+                right_exp[i] = right_b[E[i] - 1];
             }
             // XOR right_exp with key
             int[] xor_result = new int[48];
@@ -237,8 +275,74 @@ namespace SecurityLibrary.DES
             {
                 xor_result[i] = right_exp[i] ^ key_pc2[i];
             }
+            // S_box
+            int[] S_box_result = new int[32];
+            for (int S = 0, i = 0; S < 8; S++)
+            {
+                if(i + 6 <= 48)
+                {
+                    int row = Convert.ToInt32($"{xor_result[i]}{xor_result[i + 5]}", 2);
+                    int column = Convert.ToInt32($"{xor_result[i + 1]}{xor_result[i + 2]}{xor_result[i + 3]}{xor_result[i + 4]}", 2);
+                    string num = Convert.ToString(S_box(row, column, S),2);
+                    int zero_to_add = 4 - num.Length;
+                    for (int z = 0; z < zero_to_add; z++)
+                    {
+                        num = '0' + num;
+                    }
+                    for (int j = 0; j < 4; j++)
+                    {
+                        if (num[j] == '1')
+                            S_box_result[(S * 4) + j] = 1;
+                        else if (num[j] == '0')
+                            S_box_result[(S * 4) + j] = 0;
+                    }
+                }
+                i += 6;
+            }
+            // P premutation
+            int[] premutation_result = new int[32];
+            for (int i = 0; i < 32; i++)
+            {
+                premutation_result[i] = S_box_result[P[i] - 1];
+            }
+            // XOR with left side
+            int[] xor_result2 = new int[32];
+            for (int i = 0; i < 32; i++)
+            {
+                xor_result2[i] = left_b[i] ^ premutation_result[i];
+            }
+            // left side = old right side and xor_result2 is the new right side
+            for (int i = 0; i < 32; i++)
+            {
+                result[i] = right_b[i];
+                result[i + 32] = xor_result2[i];
+            }
 
-            return null;
+            return result;
+        }
+        public int S_box(int row, int column,int S)
+        {
+            switch (S)
+            {
+                case 0:
+                    return S1[(row*16) + column];
+                case 1:
+                    return S2[(row * 16) + column];
+                case 2:
+                    return S3[(row * 16) + column];
+                case 3:
+                    return S4[(row * 16) + column];
+                case 4:
+                    return S5[(row * 16) + column];
+                case 5:
+                    return S6[(row * 16) + column];
+                case 6:
+                    return S7[(row * 16) + column];
+                case 7:
+                    return S8[(row * 16) + column];
+                default:
+                    return -1;
+            }
         }
     }
 }
